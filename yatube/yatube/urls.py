2c132cc50b91093db810a8d694ae8f14ee78377f
t0pdog <topillustrated@gmail.com>
2022-05-28 00:28:42 +0300

    Коммит номер 1

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -32,6 +32,10 @@ urlpatterns = [
 ]
 
 handler404 = 'core.views.page_not_found'
+handler403csrf = 'core.views.csrf_failure'
+handler403 = 'core.views.permission_denied'
+handler500 = 'core.views.server_error'
+
 
 if settings.DEBUG:
     urlpatterns += static(
